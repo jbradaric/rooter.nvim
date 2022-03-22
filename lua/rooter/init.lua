f55689914dee0a8a6b17db8d9c47b799838afe48
Jurica Bradaric <jbradaric@gmail.com>
2022-03-22 23:31:08 +0100

    Make the plugin more configurable

--- lua/rooter/init.lua
+++ lua/rooter/init.lua
@@ -2,48 +2,55 @@ local cmd = vim.api.nvim_command
 local utils = require('rooter/utils')
 
 local M = {}
+local default_config = {
+  patterns = {'.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'CMakeLists.txt', 'package.json', 'Cargo.toml'},
+  cd_command = 'cd',
+  change_dir_for_non_project_files = '',
+  chdir_on_buf_enter = true,
+  vim_rooter_compat = true,
+}
+local config = {}
 
-local default_patterns = {'.git', '.hg'}
+local function merge_config(default, user_conf)
+  if user_conf == nil then
+    user_conf = {}
+  end
+  config = vim.tbl_deep_extend('error', {}, default)
+  config = vim.tbl_extend('force', config, user_conf)
+  return config
+end
 
-local function initialize()
-  cmd('augroup rooter')
-  cmd('autocmd!')
-  cmd('autocmd BufReadPost,BufEnter *.py call Rooter()')
-  cmd('augroup END')
+local function initialize(config)
+  if config.chdir_on_buf_enter then
+    cmd('augroup rooter')
+    cmd('autocmd!')
+    cmd('autocmd BufReadPost,BufEnter * lua require("rooter").update_cwd()')
+    cmd('augroup END')
+  end
 
-  cmd([[
-    function! Rooter()
-      execute "cd " . v:lua.require'rooter'.find_root(expand('%:p'))
-    endfunction
-  ]])
+  if config.vim_rooter_compat then
+    cmd([[
+      function! FindRootDirectory()
+        return v:lua.require'rooter'.find_root(expand('%:p'))
+      endfunction
+    ]])
 
-  cmd([[
-    function! FindRootDirectory()
-      return v:lua.require'rooter'.find_root(expand('%:p'))
-    endfunction
-  ]])
-
-  cmd('command Rooter call Rooter()')
+    cmd('command! Rooter lua require("rooter").update_cwd()')
+  end
 end
 
 function M.setup(opts)
-  initialize()
-
-  local patterns = opts and opts['patterns']
-  if not patterns then
-    patterns = default_patterns
-  end
-  M.patterns = patterns
+  config = merge_config(default_config, opts)
+  initialize(config)
 end
 
-local function find_root(path, patterns)
+local function find_root(path, config)
   local parent = utils.dirname(path)
-  local result = false
   while true do
     if not parent or parent == '' or parent == '/' then
       break
     end
-    for _, pat in ipairs(patterns) do
+    for _, pat in ipairs(config.patterns) do
       if utils.path_exists(parent .. '/' .. pat) then
         return parent
       end
@@ -52,9 +59,16 @@ local function find_root(path, patterns)
   end
 
   if not path or path == '' then
-    return os.getenv('HOME')
-  else
+    return nil
+  end
+
+  local behavior = config['change_dir_for_non_project_files'] or ''
+  if behavior == '' then
+    return nil
+  elseif behavior == 'current' then
     return utils.dirname(path)
+  elseif behavior == 'home' then
+    return os.getenv('HOME')
   end
 end
 
@@ -63,8 +77,15 @@ function M.find_root(path)
     local current = vim.api.nvim_get_current_buf()
     path = vim.api.nvim_buf_get_name(tonumber(current))
   end
-  local root = find_root(path, M.patterns)
-  return root
+  return find_root(path, config)
+end
+
+function M.update_cwd()
+  local path = vim.fn.expand('%:p')
+  local root_dir = M.find_root(path)
+  if root_dir then
+    cmd('execute "' .. config.cd_command .. ' ' .. root_dir .. '"')
+  end
 end
 
 return M
